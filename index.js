bc2e10d90f864504c046377ae6c9219ae3834e91
Austin Rognes <acrognes@iastate.edu>
2020-02-05 15:42:28 -0600

    tweaks

--- index.js
+++ index.js
@@ -11,18 +11,9 @@ document.addEventListener('touchmove', () => {
 
 function shakeLinks(elem) {
   shakeLinksTime(elem.parentElement.children[1], elem.parentElement.children[2], 0);
-  //let link1 = elem.parentElement.children[1];
-  //let link2 = elem.parentElement.children[2];
-  //link1.style.transform = "rotate(20)";
-  //link1.style.transform = "rotate(" + Math.sin(t/5) * 20 + ")";
-  //link2.style.transform = "rotate(" + Math.cos(t/5) * 20 + ")";
-  //console.log(Math.sin(t/5) * 20);
-  //t++;
-  //if(t < 40) setTimeout(shakeLinks, 5, link1, link2, t);
 }
 
 function shakeLinksTime(link1, link2, t) {
-  console.log(link1.nodeName);
   if(link1.nodeName == 'A') link1.style.transform = "rotate(" + -Math.sin(t/10) * 20 + "deg)";
   if(link2.nodeName == 'A') link2.style.transform = "rotate(" + Math.sin(t/10) * 20 + "deg)";
   t++;
