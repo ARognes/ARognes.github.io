f840af5327ffc9bb113b9df9542b3d60ea78de9a
ARognes <aurognes@gmail.com>
2022-09-02 20:23:50 -0500

    Add cicd

---
+++ actions/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const { google } = require('googleapis');
+require('dotenv').config()
+
+const resumeId = '1pWZyMr6CMDhBaMISAWG7zYhaK6WMKxRhKsb9JU1hY5s'
+
+const KEY = process.env['key'].replace(/\\n/g, '\n');
+const EMAIL = process.env['email'];
+
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+
+async function exportPDF(fileId, dest) {
+
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      private_key: KEY,
+      client_email: EMAIL
+    },
+    scopes: SCOPES
+  });
+
+  const service = google.drive({
+    version: 'v3',
+    auth
+  });
+  
+  const fsDest = fs.createWriteStream(dest);
+  return await service.files.export({
+    fileId,
+    mimeType: 'application/pdf'
+  }, { responseType: 'stream' }, (err, res) => {
+    if (err) console.error(err);
+    else res.data.on('error', console.error).pipe(fsDest);
+  });
+}
+
+exportPDF(resumeId, '../Austin Rognes - Resume.pdf');
